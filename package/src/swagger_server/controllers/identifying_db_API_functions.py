d5da754bda03edfae6be40f4996efcc59f4bd619
sabinebertram <sabine.bertram@mailbox.org>
2018-07-10 18:24:41 +0200

    adds return values

--- package/src/swagger_server/controllers/identifying_db_API_functions.py
+++ package/src/swagger_server/controllers/identifying_db_API_functions.py
@@ -1,8 +1,10 @@
 from .creation_functions import (_bulk_create_student_entries,
                                  _create_new_student_entry,
                                  _create_university_collection)
-from .getter_functions import _get_students_by_trait, _get_students_by_trait_regex
-from .update_functions import _update_trait, _update_id
+from .getter_functions import (_get_all_students, _get_student_by_id,
+                               _get_students_by_trait,
+                               _get_students_by_trait_regex)
+from .update_functions import _update_id, _update_trait
 
 
 def create_university_collection(body):
@@ -15,34 +17,37 @@ def bulk_create_student_entries(body):
     return _bulk_create_student_entries(body.get('university_name'), body.get('students_data'))
 
 def update_id(body):
-    _update_id(body.get('universtiy_name'), body.get('id'))
+    return _update_id(body.get('university_name'), body.get('id'))
 
 def update_address(body):
-    _update_trait(body.get('universtiy_name'), 'address', body.get('address'), body.get('id'))
+    return _update_trait(body.get('university_name'), 'address', body.get('address'), body.get('id'))
 
 def get_students_by_first_name(university_name, first_name):
-    _get_students_by_trait(university_name, 'first_name', first_name)
+    return _get_students_by_trait(university_name, 'first_name', first_name)
 
 def get_students_by_last_name(university_name, last_name):
-    _get_students_by_trait(university_name, 'last_name', last_name)
+    return _get_students_by_trait(university_name, 'last_name', last_name)
 
 def get_students_by_gender(university_name, gender):
-    _get_students_by_trait(university_name, 'gender', gender)
+    return _get_students_by_trait(university_name, 'gender', gender)
 
 def get_students_by_race(university_name, race):
-    _get_students_by_trait(university_name, 'race', race)
+    return _get_students_by_trait(university_name, 'race', race)
 
 def get_students_by_student_id(university_name, student_id):
-    _get_students_by_trait(university_name, 'student_id', student_id)
+    return _get_students_by_trait(university_name, 'student_id', student_id)
 
 def get_students_by_db_id(university_name, db_id):
-    _get_students_by_trait(university_name, '_id', db_id)
+    return _get_student_by_id(university_name, db_id)
 
 def get_students_by_address(university_name, address):
-    _get_students_by_trait_regex(university_name, 'address', address)
+    return _get_students_by_trait_regex(university_name, 'address', address)
 
 def get_students_by_birth_year(university_name, birth_year):
-    _get_students_by_trait_regex(university_name, 'date_of_birth', birth_year + '-')
+    return _get_students_by_trait_regex(university_name, 'date_of_birth', birth_year + '-')
 
 def get_students_by_birth_month(university_name, birth_month):
-    _get_students_by_trait_regex(university_name, 'date_of_birth', '-' + birth_month + '-')
+    return _get_students_by_trait_regex(university_name, 'date_of_birth', '-' + birth_month + '-')
+
+def get_all_students(university_name):
+    return _get_all_students(university_name)

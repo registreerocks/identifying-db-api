c118b9fbe6cb7cad0b1c9dce9681df006d73d84e
sabinebertram <sabine.bertram@mailbox.org>
2018-09-06 13:15:37 +0200

    implements access control

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,34 @@
 FROM python:3.6
 
+#SSL
+# RUN openssl req \
+#     -new \
+#     -newkey rsa:4096 \
+#     -days 365 \
+#     -nodes \
+#     -x509 \
+#     -subj "/C=ZA/ST=WC/L=CapeTown/O=Registree/CN=www.registree.rocks" \
+#     -keyout server.key \
+#     -out server.cert
+
+# connexion
+RUN mkdir -p /usr/src
+COPY oas3.zip /usr/src
+WORKDIR /usr/src
+
+RUN  apt-get update -y && \
+     apt-get upgrade -y && \
+     apt-get install unzip -y 
+
+RUN unzip oas3.zip
+RUN mv connexion-oas3 connexion
+WORKDIR connexion
+RUN pip install -e .
+
+# API
 RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
-
 COPY ./package /usr/src/app
-
+WORKDIR /usr/src/app
 RUN pip3 install -e .
 
 EXPOSE 8080
--- package/src/swagger_server/controllers/identifying_db_API_functions.py
+++ package/src/swagger_server/controllers/identifying_db_API_functions.py
@@ -1,3 +1,4 @@
+from .authentication import requires_auth, requires_scope
 from .creation_functions import (_bulk_create_student_entries,
                                  _create_new_student_entry,
                                  _create_university_collection)
@@ -7,50 +8,82 @@ from .getter_functions import (_bulk_get_students_by_id, _get_all_students,
 from .update_functions import _update_id, _update_trait
 
 
+@requires_auth
+@requires_scope('registree')
 def create_university_collection(body):
     return _create_university_collection(body.get('university_name'))
 
+@requires_auth
+@requires_scope('admin')
 def create_new_student_entry(body):
     return _create_new_student_entry(body.get('university_name'), body.get('student_data'))
 
+@requires_auth
+@requires_scope('admin')
 def bulk_create_student_entries(body):
     return _bulk_create_student_entries(body.get('university_name'), body.get('students_data'))
 
+@requires_auth
+@requires_scope('student')
 def update_id(body):
     return _update_id(body.get('university_name'), body.get('id'))
 
+@requires_auth
+@requires_scope('student')
 def update_address(body):
     return _update_trait(body.get('university_name'), 'address', body.get('address'), body.get('id'))
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_first_name(university_name, first_name):
     return _get_students_by_trait(university_name, 'first_name', first_name)
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_last_name(university_name, last_name):
     return _get_students_by_trait(university_name, 'last_name', last_name)
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_gender(university_name, gender):
     return _get_students_by_trait(university_name, 'gender', gender)
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_race(university_name, race):
     return _get_students_by_trait(university_name, 'race', race)
 
+@requires_auth
+@requires_scope('admin', 'lecturer')
 def get_students_by_student_id(university_name, student_id):
     return _get_students_by_trait(university_name, 'student_id', student_id)
 
+@requires_auth
+@requires_scope('admin', 'lecturer', 'student')
 def get_student_by_db_id(university_name, db_id):
     return _get_student_by_id(university_name, db_id)
 
+@requires_auth
+@requires_scope('admin', 'lecturer')
 def bulk_get_students_by_id(body):
     return _bulk_get_students_by_id(body.get('university_name'), body.get('db_ids'))
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_address(university_name, address):
     return _get_students_by_trait_regex(university_name, 'address', address)
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_birth_year(university_name, birth_year):
     return _get_students_by_trait_regex(university_name, 'date_of_birth', birth_year + '-')
 
+@requires_auth
+@requires_scope('admin')
 def get_students_by_birth_month(university_name, birth_month):
     return _get_students_by_trait_regex(university_name, 'date_of_birth', '-' + birth_month + '-')
 
+@requires_auth
+@requires_scope('admin')
 def get_all_students(university_name):
     return _get_all_students(university_name)

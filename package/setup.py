df6eed91426b88e0f19ad55b7d11bdd0be0e1212
Sabine <sabine.bertram@mailbox.org>
2020-05-14 11:01:30 +0200

    1.1.2

--- package/setup.py
+++ package/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='identifying_db_API',
-   version='1.1.1',
+   version='1.1.2',
    maintainer='Sabine Bertram',
    maintainer_email='sabine.bertram@mailbox.org',
    package_dir={'': 'src'},
